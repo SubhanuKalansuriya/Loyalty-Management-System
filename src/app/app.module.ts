46f2dbf5e5ed49d02d4d7f31b1db2e7e76db26c2
Subhanu Kalansuriya <53854090+SubhanuKalansuriya@users.noreply.github.com>
2021-07-15 16:23:50 +0530

    deleted 2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegistrationComponent
+    
   ],
   imports: [
     BrowserModule,
